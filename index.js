a46db39b34f91452acc12d3161fd5a8b89efce29
Fahmi Kariem <160807264+fahmikarim@users.noreply.github.com>
2025-05-24 00:13:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import route from "./routes/NotesRoute.js";  // sesuaikan nama filenya jika beda
 import "./models/index.js";  // import model agar relasi dan sinkronisasi jalan
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const app = express();
@@ -13,7 +12,8 @@ const port = process.env.PORT || 5000;
 // Konfigurasi CORS dengan whitelist origin dan opsi credentials
 const corsOptions = {
   origin: [
-    "http://localhost:3000",
+    "http://localhost:3000", // untuk development
+    "https://frontend-fahmi-028-dot-a-08-450504.uc.r.appspot.com" // untuk production
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
@@ -25,7 +25,6 @@ app.options("*", cors(corsOptions)); // pre-flight request
 
 app.use(express.json());
 app.use(cookieParser());  // supaya bisa baca req.cookies
-
 app.use(route);  // pakai route dari NotesRoute.js
 
 app.listen(port, () => {
